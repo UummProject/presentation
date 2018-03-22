772219ece786ea50c075c3a403afc9af9976562a
Xavi Vives <xavings@gmail.com>
2018-03-22 09:52:13 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Impress, Step } from 'react-impressjs';
-import 'react-impressjs/styles/react-impressjs.css';
+import './styles.css';
+
+const TRANSITION_TIME = 500
+const WIDTH = window.innerWidth * 1.2
 
 class App extends Component {
   render() {
@@ -12,20 +15,32 @@ class App extends Component {
           progress={true}
           fallbackMessage={<p>Sorry, your <b>device or browser</b> couldn't support well.</p>}
           >
-          <Step id={'overview'} /> 
-          <Step id={'any_id'} className={'class_name'} />
-          <Step className={'without_id_is_ok'} 
-                data={
-                  {
-                      x:100,
-                      y:-100,
-                      scale:2
-                  }}/>
-          <Step duration={1500}>
-              <h1>Any Element write in Step!</h1>
-              <hr />
-              <p>Made by your <b>Creativity</b> !!</p>
+         
+          <Step 
+            id = "1"
+            //className={'without_id_is_ok'} 
+            data={{ x:0, y:0, scale:1 }}>
+            <h1>What's this about?</h1>
+            
+            <p> Redesigning governance</p>
           </Step>
+
+
+          <Step
+            duration={TRANSITION_TIME}
+            data={
+            {
+                x:WIDTH*0,
+                y:0,
+                scale:1
+            }}>
+            
+            <h1>Unga bunga!</h1>
+          </Step>
+
+          
+
+         
       </Impress>
       </div>
     );
